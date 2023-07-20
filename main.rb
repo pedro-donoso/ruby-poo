7e4bfed68112f9949465c5b56423621de5bb2d27
pedro-donoso <pedro.donoso.r@usach.cl>
2023-07-20 13:17:02 -0400

    defino clases

--- main.rb
+++ main.rb
@@ -1,30 +1,56 @@
 class Person
-def initialize(first, last, age, type)
-@first_name = first
-@last_name = last
-@age = age
-@type = type
+    def initialize(first, last, age, type)
+                @first_name = first
+                @last_name = last
+                @age = age
+                @type = type
+    end
+    def birthday
+                @age += 1
+    end
+
+    #metodos
+    def talk
+                if @type == "Student"
+                puts "Aquí es la clase de programación con Ruby?"
+
+                elsif @type == "Teacher"
+                puts "Bienvenidos a la clase de programación con Ruby!"
+
+                elsif @type == "Parent"
+                puts "Aquí es la reunión de apoderados?"
+        end
+    end
+
+    def introduce
+            if @type == "Student"
+            puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+
+            elsif @type == "Teacher"
+            puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+
+            elsif @type == "Parent"
+            puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}
+            #{@last_name}."
+        end
+    end
 end
-def birthday
-@age += 1
+
+#defino clases
+class
+    Student < Person
 end
-def talk
-if @type == "Student"
-puts "Aquí es la clase de programación con Ruby?"
-elsif @type == "Teacher"
-puts "Bienvenidos a la clase de programación con Ruby!"
-elsif @type == "Parent"
-puts "Aquí es la reunión de apoderados?"
+
+class
+    Teacher < Person
 end
+
+class
+    Parent < Person
 end
-def introduce
-if @type == "Student"
-puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
-elsif @type == "Teacher"
-puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-elsif @type == "Parent"
-puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}
-#{@last_name}."
-end
-end
-end
+
+# llamo metodo
+# persona = Person.new('Juan', 'Pérez', 18, 'Student')
+# persona.talk
+
+# puts persona
