b15b2b6690f5b7b01a0344505efe903d4f4f2428
pedro-donoso <pedro.donoso.r@usach.cl>
2023-07-20 12:37:53 -0400

    agrego código

---
+++ main.rb
@@ -0,0 +1,30 @@
+class Person
+def initialize(first, last, age, type)
+@first_name = first
+@last_name = last
+@age = age
+@type = type
+end
+def birthday
+@age += 1
+end
+def talk
+if @type == "Student"
+puts "Aquí es la clase de programación con Ruby?"
+elsif @type == "Teacher"
+puts "Bienvenidos a la clase de programación con Ruby!"
+elsif @type == "Parent"
+puts "Aquí es la reunión de apoderados?"
+end
+end
+def introduce
+if @type == "Student"
+puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+elsif @type == "Teacher"
+puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+elsif @type == "Parent"
+puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}
+#{@last_name}."
+end
+end
+end
