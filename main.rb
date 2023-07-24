617c2e8874375ea29192ae5cffcd79bf3770cd49
pedro-donoso <pedro.donoso.r@usach.cl>
2023-07-24 11:19:28 -0400

    refactorizo código

--- main.rb
+++ main.rb
@@ -10,23 +10,39 @@ class Person
     end
 end
 
-def talk
-        if @type == "Student"
-            puts "Aquí es la clase de programación con Ruby?"
-        elsif @type == "Teacher"
-            puts "Bienvenidos a la clase de programación con Ruby!"
-        elsif @type == "Parent"
-            puts "Aquí es la reunión de apoderados?"
-        end
+class Student < Person
+    def talk
+        puts 'Aquí es la clase de programación con Ruby?'
     end
 
     def introduce
-        if @type == "Student"
-            puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
-        elsif @type == "Teacher"
-            puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-        elsif @type == "Parent"
-            puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}#{@last_name}."
-        end
+        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
     end
 end
+
+class Teacher < Person
+    def talk
+        puts 'Bienvenidos a la clase de programación con Ruby!'
+    end
+
+    def introduce
+        puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Parent < Person
+    def talk
+        puts 'Aquí es la reunión de apoderados?'
+    end
+
+    def introduce
+        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}
+        #{@last_name}."
+    end
+end
+
+# llamo al método talk
+persona = Student.new('Juan', 'Perez', 18)
+persona.introduce
+
+puts persona
