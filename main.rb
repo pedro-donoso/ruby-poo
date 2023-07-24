95a6c3e082be6063d02f3cb9886f021d08d836e0
pedro-donoso <pedro.donoso.r@usach.cl>
2023-07-24 10:34:51 -0400

    reinicio

--- main.rb
+++ main.rb
@@ -1,48 +1,32 @@
 class Person
-        def initialize(first, last, age)
-                        @first_name = first
-                        @last_name = last
-                        @age = age
-        end
-
-        def birthday
-                        @age += 1
-        end
-
-        #defino clases y métodos por separado
-            class Student < Person
-            def talk
-                            puts "Aquí es la clase de programación con Ruby?"
-            end
-
-            class Teacher < Person
-            def talk
-                            puts "Bienvenidos a la clase de programación con Ruby!"
-            end
-
-            class Parent < Person
-            def talk
-                            puts "Aquí es la reunión de apoderados?"
+    def initialize(first, last, age, type)
+        @first_name = first
+        @last_name = last
+        @age = age
+        @type = type
     end
 
-        class Student < Person
-        def introduce
-        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
-        end
-
-        class Teacher < Person
-        def introduce
-        puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
-        end
-
-        class Parent < Person
-        def introduce
-        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."
+    def birthday
+        @age += 1
     end
-end
 
-#datos de entrada
-persona_uno = Person.new('Juan', 'Pérez', 18)
-puts persona_uno.talk
-end
+    def talk
+        if @type == "Student"
+            puts "Aquí es la clase de programación con Ruby?"
+        elsif @type == "Teacher"
+            puts "Bienvenidos a la clase de programación con Ruby!"
+        elsif @type == "Parent"
+            puts "Aquí es la reunión de apoderados?"
+        end
+    end
+
+    def introduce
+        if @type == "Student"
+            puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+        elsif @type == "Teacher"
+            puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+        elsif @type == "Parent"
+            puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name}#{@last_name}."
+        end
+    end
 end
